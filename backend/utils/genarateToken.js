241c6397d0c476a86a4dfc2f3aba23c886670926
Vishnu Syamala <vvishnusyamala@gmail.com>
2024-06-16 10:23:20 +0530

    changed paths

--- backend/utils/genarateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generteToken = (id) => {
-    return jwt.sign({ id }, process.env.JWT_SECRET, {
-        expiresIn: '30d'
-    })
-}
-
-export default generteToken
